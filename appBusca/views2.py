e8b94e4f55e1d060dc47b7211668b4b7d0ca8a54
admin.soares <davifsoares@gmail.com>
2024-09-20 22:32:33 -0300

    9

--- appBusca/views2.py
+++ appBusca/views2.py
@@ -2,25 +2,27 @@ from django.shortcuts import render
 from .forms import BuscaForm
 from .models import Item
 import json
-
+from django.http import HttpResponse, JsonResponse
 def busca(request):
     form = BuscaForm(request.GET or None)
-    itens = Item.objects.all()
 
-    if form.is_valid():
-        query = form.cleaned_data['query']
-        itens = itens.filter(nome_item__istartswith=query)
-
-    # Para depuração, se necessário
-    print("Itens encontrados:", itens)
-
-    context = {
-        'form': form,
-        'itens': itens,
-        'itens_json': json.dumps(list(itens.values('cod_item', 'nome_item', 'comp_ativ_itm')))
+    if request.method == 'POST':
+        nome = request.POST.get('busca')
+        itens = Item.objects.filter(nome_item__icontains=nome)
+        context = {
+            'form': form,
+            'itens': itens,
+            'itens_json': json.dumps(list(itens.values('cod_item', 'nome_item', 'comp_ativ_itm')))
     }
 
-    return render(request, 'busca.html', context)
+        return render(request, 'busca.html', context)
+    return  render(request, 'busca.html')
 
 def medicamento(request, cod_item):
-    return
+    cod_item = int(cod_item)
+    item = Item.objects.filter(cod_item=cod_item)
+    context = {
+        'item': item,
+        'itens_json': json.dumps(list(item.values('cod_item', 'nome_item', 'comp_ativ_itm')))
+    }
+    return render(request, 'produto.html', context)
