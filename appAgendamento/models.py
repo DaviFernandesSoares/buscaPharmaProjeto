b24065748ee92b15499188773e2e90539895b9bc
37223 <matheusrocham.ms@gmai.com>
2024-10-07 16:28:34 -0300

    15

--- appAgendamento/models.py
+++ appAgendamento/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from appBusca.models import Unidade
+from appCadUsuario.models import Usuario
+
+
+class Agendamento(models.Model):
+    STATUS_CHOICES = (
+        ('Agendado','agendado'),
+        ('Cancelado','cancelado'),
+
+    )
+    id_agendamento = models.AutoField(primary_key=True)
+    id_unidade = models.ForeignKey(Unidade, on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Agendado')
+    data = models.DateField(auto_now=False, auto_now_add=True)
+    hora = models.TimeField(auto_now=False, auto_now_add=True)
+
+    class Meta:
+        db_table = 'agendamento'
--- busca.js
+++
@@ -1,6 +0,0 @@
-const remedios = document.getElementById("tableRemedios")
-listaRemedios.forEach((item)=>{
-    const tr = document.createElement("tr")
-    tr.innerHTML = `<td class="td"><a href="produto.html?id=${item.id}">${item.name}</a></td><td>${item.description}</td>`
-    remedios.appendChild(tr);
-})
